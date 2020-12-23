4466caac9589b7684e2faf4ab059d344babeeb2f
aliazim <a.azimoshan@gmail.com>
2020-12-23 16:05:07 +0330

    first commit

---
+++ android/app/src/main/kotlin/com/example/fade_in_scroll/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fade_in_scroll
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,16 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:device_info_plus_web/device_info_plus_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  DeviceInfoPlusPlugin.registerWith(registry.registrarFor(DeviceInfoPlusPlugin));
+  registry.registerMessageHandler();
+}
